9dad35f3d53c770669043530032cfe99a558bc48
xkloveme <xkloveme@gmail.com>
2023-05-17 07:32:16 +0000

    Deploying to gh-pages from @ xkloveme/135edit-monkey@31e44279908d7320d1054232dec68f144ec542d6 🚀

--- 135edit-monkey.user.js
+++ 135edit-monkey.user.js
@@ -1,14 +1,18 @@
 // ==UserScript==
 // @name         135编辑器VIP采集
 // @namespace    http://tampermonkey.net/
-// @version      0.0.0
+// @version      1.0.0
 // @author       xkloveme
-// @description  在用户打开135编辑器,获取VIP模版脚本.
+// @description  在用户打开135编辑器、96编辑器、主编编辑器、易点编辑器vip限制,获取VIP模版脚本.
 // @icon         https://www.135editor.com/favicon.ico
 // @homepage     https://github.com/xkloveme
 // @homepageURL  https://github.com/xkloveme/135edit-monkey
 // @updateURL    https://cdn.staticaly.com/gh/xkloveme/135edit-monkey/gh-pages/135edit-monkey.user.js
-// @match        https://www.135editor.com/*
+// @match        *://*.135editor.com*
+// @match        *://*.135editor.com/*
+// @match        *://bj.96weixin.com*
+// @match        *://www.wxeditor.com*
+// @match        *://www.zhubian.com*
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js
 // ==/UserScript==
 
@@ -16,15 +20,42 @@
   'use strict';
 
   window.onload = function() {
+    var host = window.location.host;
+    switch (host) {
+      case "www.135editor.com":
+        styleClick();
+        break;
+      case "bj.96weixin.com":
+        hack_96_ZB();
+        break;
+      case "www.zhubian.com":
+        hack_96_ZB();
+        break;
+      case "www.wxeditor.com":
+        hackED();
+        break;
+    }
     function styleClick() {
       $(".editor-template-list > li").each(function() {
         $(this).attr("goumai", 1);
         $(this).attr("data-paid", 1);
-        $(this).attr("data-id", 126887);
+        $(this).attr("data-id", "126887");
         $(this).removeClass("vip-style");
       });
     }
-    styleClick();
+    function hack_96_ZB() {
+      $("div").click(function(e) {
+        $(".rich_media_content").attr("data-vip", "1");
+      });
+    }
+    function hackED() {
+      $("div").click(function(e) {
+        $(".yead_editor").attr("data-use", "1");
+      });
+    }
+    window.style_click = window.show_role_vip_dialog = function() {
+    };
+    window.loged_user = 1;
     window.get_vip_roles = function() {
       return [9];
     };
